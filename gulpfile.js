d69c559c7a1230608c09c011c9edde15eab58870
kristenmills <kristen@kristen-mills.com>
2015-01-05 13:01:19 -0500

    Removed hard tabs

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,18 @@ var app = require('./server/server');
 
 
 var paths = {
-	client_root: './app/',
-	js_root: './app/js/',
-	js_files: './app/**/*.js',
-	js_entrypoint: './app/js/main.js',
-	build_dir: './dist/',
-	react_root: './app/bower_components/react',
-	fluxxor_root: './app/bower_components/fluxxor'
+  client_root: './app/',
+  js_root: './app/js/',
+  js_files: './app/**/*.js',
+  js_entrypoint: './app/js/main.js',
+  build_dir: './dist/',
+  react_root: './app/bower_components/react',
+  fluxxor_root: './app/bower_components/fluxxor'
 }
 
 gulp.task('compile-scripts', function(){
   var b = browserify({
-  	paths: [paths.react_root, paths.fluxxor_root]
+    paths: [paths.react_root, paths.fluxxor_root]
   });
   b.transform(reactify); // use the reactify transform
   b.add(paths.js_entrypoint);
@@ -28,7 +28,7 @@ gulp.task('compile-scripts', function(){
 
 
 gulp.task('watch-scripts', function(){
-	gulp.watch(paths.js_files, ['compile-scripts']);
+  gulp.watch(paths.js_files, ['compile-scripts']);
 });
 
 gulp.task('start-server', function(){
