66d924e411136f52db802c76d947c0a1e1eb54a7
kristenmills <kristen@kristen-mills.com>
2015-01-05 13:51:30 -0500

    Added copy task and css minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,19 @@ var gulp = require('gulp');
 var source = require("vinyl-source-stream");
 var reactify = require('reactify');
 var app = require('./server/server');
-
+var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 var paths = {
   client_root: './app/',
   js_root: './app/js/',
   js_files: './app/**/*.@(js|jsx)',
+  other_files: './app/**/*.!(js|jsx|css)',
   js_entrypoint: './app/js/main.js',
+  css_files: './app/css/**/*.css',
   build_dir: './dist/',
   react_root: './app/bower_components/react',
-  fluxxor_root: './app/bower_components/fluxxor'
+  fluxxor_root: './app/bower_components/fluxxor',
 }
 
 gulp.task('compile-scripts', function(){
@@ -27,9 +30,16 @@ gulp.task('compile-scripts', function(){
     .pipe(gulp.dest(paths.build_dir));
 });
 
+gulp.task('copy', function(){
+  gulp.src([paths.other_files, '!./app/bower_components/**/*'])
+    .pipe(gulp.dest(paths.build_dir));
+});
 
-gulp.task('watch-scripts', function(){
-  gulp.watch(paths.js_files, ['compile-scripts']);
+gulp.task('minify:css', function(){
+  gulp.src(paths.css_files)
+    .pipe(concat('app.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(paths.build_dir));
 });
 
 gulp.task('start-server', function(){
@@ -40,4 +50,10 @@ gulp.task('start-server', function(){
   });
 });
 
-gulp.task('default', ['compile-scripts', 'start-server', 'watch-scripts']);
+gulp.task('watch-scripts', function(){
+  gulp.watch(paths.js_files, ['compile-scripts']);
+  gulp.watch([paths.other_files, '!./app/bower_components/**/*'], ['copy']);
+  gulp.watch(paths.css_files,['minify:css']);
+});
+
+gulp.task('default', ['compile-scripts', 'copy', 'minify:css', 'start-server', 'watch-scripts']);
