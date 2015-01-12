32dcc57cd4357fefbf769163ebc72570cd6fd1a7
Wesley Wigham <wwigham@gmail.com>
2015-01-12 15:29:50 -0500

    fix the less task zomg

--- gulp/config.js
+++ gulp/config.js
@@ -5,7 +5,7 @@ module.exports = {
     js_files: './app/**/*.@(js|jsx)',
     js_entrypoint: './js/main.js',
     other_files: './app/**/*.!(js|jsx|css|less)',
-    less_files:'./app/**/*.@(css|less)',
+    less_files:'./app/**/*.@(less)',
     material_less: './node_modules/material-ui/src/less/', 
     build_dir: './dist/',
     test_files: './test/**/*-test.js'
