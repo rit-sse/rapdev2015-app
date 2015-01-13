f1ef6fa61bc7099def29931da269d878279f5a61
kristenmills <kristen@kristen-mills.com>
2015-01-13 12:00:47 -0500

    Update api/user

--- app/js/api/user.js
+++ app/js/api/user.js
@@ -3,11 +3,11 @@ var core = require('./core');
 var baseEndpoint = 'http://localhost:3001/api/users';
 
 var User = {
-  all: function() {
-    return core.get(baseEndpoint);
-  },
   one: function(id) {
     return core.get(baseEndpoint + '/' + id);
+  },
+  update: function(id, user) {
+    return core.put(baseEndpoint + '/' + id, user);
   },
   isSignedIn: function() {
     return new Date(core.getToken().exp) > Date.now();
