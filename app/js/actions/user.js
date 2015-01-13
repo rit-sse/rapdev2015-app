6fa3801530f08f5f563e39c9564e7de6b78e5f5e
John <john@jrenner.net>
2015-01-13 11:23:55 -0500

    User store

--- app/js/actions/user.js
+++ app/js/actions/user.js
@@ -1,10 +1,25 @@
 var actions = require('../constants/actions');
+var Token = require('../api/token');
+
 
 module.exports = {
   signIn: function(provider){
-    this.dispatch(actions.SIGN_IN_SUCCESS, {
-      firstName: 'first',
-      lastName: 'last'
-    })
+    hello(service)
+      .login({scope: 'email'})
+      .then(function(data) {
+        console.log(data);
+        console.log('looking up profile...');
+        var token = data.authResponse.access_token;
+        return Token.get(provider, token);
+        }, function(e){
+            console.log(e.error.message);
+        });
+      })
+      .then(function(){
+        this.dispatch(actions.SIGN_IN_SUCCESS, {
+            firstName: 'first',
+            lastName: 'last'
+        });
+      });
   }
 }
--- app/js/stores/User.js
+++ app/js/stores/User.js
@@ -1,5 +1,6 @@
 var Fluxxor = require('fluxxor');
 var actions = require('../constants/actions');
+var User = require('../api/user');
 
 var UserStore = Fluxxor.createStore({
   initialize: function(options){
@@ -8,16 +9,29 @@ var UserStore = Fluxxor.createStore({
 
 
     this.bindActions(
-      actions.SIGN_IN_SUCCESS, this.signIn,
-      actions.SIGN_IN_FAILURE, this.signInFail
+      actions.SIGN_IN_SUCCESS, this._signIn,
+      actions.SIGN_IN_FAILURE, this._signInFail,
+      actions.SIGN_OUT, this.signOut
     );
   },
 
-  signIn: function(payload, type){
+  _signIn: function(payload, type){
     this.data.user = {
       firstName: payload.firstName,
       lastName: payload.lastName
     }
+
+    this.emit('change');
+  },
+
+  _signOut: function(payload, type){
+    this.data = {};
+    this.emit('change');
   }
 
+  isSignedIn: function(){
+    return User.isSignedIn();
+  },
+
 });
+
