41b78f67eeff50e03838f79e9e2e1634eccbbc66
Zachary Yaro <zmyaro@gmail.com>
2015-01-20 12:47:39 -0500

    Remove test button and add to-dos panel

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -20,44 +20,18 @@ var App = React.createClass({
     return (
       <div className="app">
         <AppBar />
-        <Panel width={256} depth={1}>
+        <Panel width={224} depth={1}>
           <TagSearch />
         </Panel>
         <Panel width="rest" depth={0}>
           <p> hi </p>
-          <p><TestButton /></p>
+        </Panel>
+        <Panel width={192} depth={1}>
+          To-dos:
         </Panel>
       </div>
     );
   }
 });
-
-
-var api = require('../api'),
-  Tag = api.Tag,
-  Token = api.Token;
-
-var TestButton = React.createClass({
-  getInitialState() {
-    return {response: ''};
-  },
-  handleClick(event) {
-    Tag
-      .create({name: 'projects', color: 'FF00FF' })
-      .then(() => {
-        Tag.all().then((json) => {
-          console.log(json)
-          this.setState({response: JSON.stringify(json, null, 4)});
-        })
-        .catch((err) => {
-          this.setState({response: JSON.stringify(err, null, 4)});
-        });
-      });
-  },
-  render() {
-    return <div><button onClick={this.handleClick} className="z1">Fetch a thing!</button><br/><span><pre>{this.state.response}</pre></span></div>
-  }
-});
-
 
 module.exports = App;
