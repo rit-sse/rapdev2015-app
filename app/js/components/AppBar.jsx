78a5abaa12409f54d02986b92602b464857c1f07
Zachary Yaro <zmyaro@gmail.com>
2015-01-20 12:46:20 -0500

    Add theme colors

--- app/js/components/AppBar.jsx
+++ app/js/components/AppBar.jsx
@@ -5,7 +5,7 @@ var LogIn = require('./LogIn');
 var AppBar = React.createClass({
   render() {
     return (
-      <header className="appBar z1">
+      <header className="appBar toolbar z1">
         <LogIn />
         RapDev 2015
       </header>
